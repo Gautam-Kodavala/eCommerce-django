0fbeb8a6218305d714609e142c9545c95647eb60
Gautam-kodavala <kodavlagautam29@gmail.com>
2023-10-09 19:01:24 +0530

    first vidio complate

--- products/models.py
+++ products/models.py
@@ -14,4 +14,9 @@ class product(BaseModel):
     product_name = models.CharField(max_length=100)
     category = models.ForeignKey(category , on_delete=models.CASCADE , related_name="products")
     price = models.IntegerField()
+    product_description = models.TextField()
+
+class ProductImage(BaseModel):
+    product = models.ForeignKey(product , on_delete=models.CASCADE , related_name="product_image")
+    image = models.ImageField(upload = "product")
     
